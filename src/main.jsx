0b1e3c1f0ff8ae3df5a559fc430ef59d8d249d7b
onyedkichi23 <ozohenrywk@gmail.com>
2024-08-28 15:23:13 +0100

    Initialize Chakra UI and theming

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/** @format */
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
+import themes from "./styles/themes.js";
+
+const theme = extendBaseTheme({ themes });
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		{/* ChakraBaseProvider to use Chakra UI library */}
+		<ChakraBaseProvider theme={theme}>
+			<App />
+		</ChakraBaseProvider>
+	</StrictMode>
+);
