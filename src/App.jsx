275f11fe991b9c996eb56d1f70014e30bbbdeeaa
onyedkichi23 <ozohenrywk@gmail.com>
2024-09-09 08:14:59 +0100

    removed unused import

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@
 
 import { Box } from "@chakra-ui/react";
 import "./App.css";
+import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
 import MainSection from "./components/main/main-section";
-import Footer from "./components/footer/footer";
-import ProjectTabsContextProvider from "./context-providers/nav-tabs-ref-provider/nav-tabs-ref-provider";
 
 function App() {
 	return (
